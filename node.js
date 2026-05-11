a950ce2823b32e95c01a8a5d6cac9794da66e43b
F1xBoy <fxboy4352@gmail.com>
2026-05-11 21:51:57 +0500

    Update node.js

--- node.js
+++ node.js
@@ -17,8 +17,7 @@ async function runAI() {
 
         try {
             // Запрос напрямую к Gemini API
-           const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-001:generateContent?key=${API_KEY}`, {
-                method: "POST",
+            const response = await fetch(`https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`, {                method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
